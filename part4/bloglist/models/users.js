1ebca84d358b47016e551bd33994e8819bc728ae
Tu Vu <tuvu852@gmail.com>
2022-01-03 21:34:02 -0600

    Add user model

---
+++ part4/bloglist/models/users.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose"); // ODM
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    minLength: 5,
+    required: true,
+  },
+  name: {
+    type: String,
+    minLength: 5,
+    required: true,
+  },
+  passwordHash: {
+    type: String,
+    minLength: 10,
+    required: true,
+  },
+  blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Blog",
+    },
+  ],
+});
+
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    // the passwordHash should not be revealed
+    delete returnedObject.passwordHash;
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
